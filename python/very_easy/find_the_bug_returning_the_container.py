094402cc5d25d7857f32922ceb7c03330e7c773b
gianmillare <gianmillaregsm@gmail.com>
2020-06-13 11:43:58 -0700

    very easy += find the bug returning the container

---
+++ python/very_easy/find_the_bug_returning_the_container.py
@@ -0,0 +1,12 @@
+#Fix this code:
+def get_container(product):
+	matches = {
+	"Bread" : "bag",
+	"Milk" : "bottle",
+	"Beer" : "bottle",
+	"Eggs" : "carton",
+	"Cerials" : "box",
+	"Candy" : "plastic",
+	"Cheese" : None
+	}
+	return matches[product]
