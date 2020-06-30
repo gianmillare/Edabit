60b000388a9c23a003ed34cdc04c3454b205a8eb
gianmillare <gianmillaregsm@gmail.com>
2020-06-29 18:28:04 -0700

    fixing syntax error

--- python/medium/how_many_solutions_does_this_quadratic_have.py
+++ python/medium/how_many_solutions_does_this_quadratic_have.py
@@ -2,10 +2,10 @@
 # https://edabit.com/challenge/o2AKq4xy3nfZabKXL
 
 def solutions(a, b, c):
-    D = (b**2) - (4*a*c)
-	if D > 0:
+	d = (b**2) - (4*a*c)
+	if d > 0:
 		return 2
-	elif D == 0:
+	elif d == 0:
 		return 1
 	else:
 		return 0
