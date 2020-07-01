1b1c98cd79ea99d563c50cfea6410d6473aca6e3
gianmillare <gianmillaregsm@gmail.com>
2020-06-30 18:15:24 -0700

    started javascript folder

---
+++ javascript/very_easy/convert_minutes_into_seconds.js
@@ -0,0 +1,8 @@
+// Convert Minutes into Seconds
+// https://edabit.com/challenge/8q54MKnRrm89pSLmW
+
+function convert(n){
+    return n * 60
+}
+
+console.log(convert(5))
---
+++ javascript/very_easy/return_the_sum_of_two_numbers.js
@@ -0,0 +1,8 @@
+// Return the Sum of Two Numbers
+// https://edabit.com/challenge/3LpBLgNRyaHMvNb4j
+
+function addition(x, y){
+    return x + y
+}
+
+console.log(addition(-3, -6))
